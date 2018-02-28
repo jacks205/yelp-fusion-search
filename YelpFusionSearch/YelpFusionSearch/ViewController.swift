8ca7828846d9f516cc6575ae6da9f8d244465907
jacks205 <markjacks205@gmail.com>
2018-02-27 17:10:56 -0800

    initial commit

---
+++ YelpFusionSearch/YelpFusionSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpFusionSearch
+//
+//  Created by Mark Jackson on 2/27/18.
+//  Copyright © 2018 Mark Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
